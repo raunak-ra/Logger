f50cd938ecde2499292f56f5eff53a64abe21154
Raunak <rsingh4work@gmail.com>
2019-10-19 21:30:08 +0530

    Add skeleton

---
+++ Logger/ITxsLogger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logger
+{
+    interface ITxsLogger
+    {
+        void InitLogger();
+        void LogApi(ApiLogger logger);
+        void LogTrace(TraceLogger logger);
+        void LogError(ErrorLogger logger);
+    }
+}
---
+++ Logger/TxsLogger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logger
+{
+    class TxsLogger : ITxsLogger
+    {
+        public void InitLogger()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogApi(ApiLogger logger)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogError(ErrorLogger logger)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogTrace(TraceLogger logger)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
